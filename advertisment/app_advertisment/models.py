63572a40a3fd49abf306950758b4c21ac28e80da
Юрий Голенков <yur.golenkoff@yandex.ru>
2023-08-10 17:15:22 +0300

    Код после 9 урока

--- advertisment/app_advertisment/models.py
+++ advertisment/app_advertisment/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 
+
+User = get_user_model()
 
 class Advertisement(models.Model):
     title = models.CharField('Заголовок', max_length=128)
@@ -11,11 +14,22 @@ class Advertisement(models.Model):
     auction = models.BooleanField('Торг', help_text='Отьметьте, если торг уместен')
     created_at = models.DateTimeField('Дата и время создания', auto_now_add=True)
     updated_at = models.DateTimeField('Дата и время изменения', auto_now=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('Изображение', upload_to='advertisements/')
 
     @admin.display(description='Дата создания')
     def created_date(self):
         if self.created_at.date() == timezone.now().date():
             created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.updated_at.time().strftime('%H:%M:%S')
             return format_html(
                 '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
             )
